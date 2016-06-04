95f15ade489178d13a12294af6c9a727d858717a
Mortezaipo <mortezaipo@gmail.com>
2016-06-04 16:45:48 +0430

    Update projects page + javascript and css files.

--- inc/js/extra.js
+++ inc/js/extra.js
@@ -1,17 +1,22 @@
 $(document).ready(function(){
-    $.get("https://api.github.com/users/mortezaipo/repos", function(data) {
-        $("#projects_list #load").remove();
+    $.get('https://api.github.com/users/mortezaipo/repos', function(data) {
+        $('#projects_list #load').remove();
         jQuery.each(data, function(i, v){
-            $("#projects_list tbody")
-                .append($("<tr>")
-                    .append($("<td>").append(i+1))
-                    .append($("<td>").append(v.name))
-                    .append($("<td>").append(v.description))
-                    .append($("<td>").append(v.updated_at.substring(0, 10)))
-                    .append($("<td>").append(
-                        $("<a>").attr({href: v.html_url, class: 'btn btn-xs btn-info'}).html("Source"))
-                    )
-                );
+            if(v.name != 'Stage')
+            {
+                code_fork = '';
+                if(v.fork == true) code_fork = ' <i class="fa fa-code-fork"></i>'
+                $('#projects_list tbody')
+                    .append($("<tr>")
+                        .append($('<td class="text-center">').append(i+1))
+                        .append($('<td>').append(v.name + code_fork))
+                        .append($('<td>').append(v.description))
+                        .append($('<td>').append(v.updated_at.substring(0, 10)))
+                        .append($('<td>').append(
+                            $('<a>').attr({href:v.html_url, class:'btn btn-xs btn-info btn-block', target:'_blank'}).html('<i class="fa fa-github"></i> Source'))
+                        )
+                    );
+            }
         });
     });
 });
