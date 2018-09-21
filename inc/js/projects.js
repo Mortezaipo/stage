b1e72eed7438f4adc2bed0d3cd46860b64f38b7b
Morteza Nourelahi Alamdari <me@mortezana.com>
2018-09-21 23:49:53 +0430

    Update website structure.

--- inc/js/projects.js
+++ inc/js/projects.js
@@ -1,21 +1,29 @@
 function load_projects() {
     $.get('https://api.github.com/users/mortezaipo/repos', function(data) {
-    $('#projects_list #load').remove();
+    $('#projects-list #load').remove();
     jQuery.each(data, function(i, v){
         if(v.name != 'stage') //ignore my website project.
         {
-            code_fork = '';
-            if(v.fork == true) code_fork = ' <i class="fa fa-code-fork"></i>'
-            $('#projects_list tbody')
-                .append($("<tr>")
-                    .append($('<td class="vertical-center text-center">').append(i+1))
-                    .append($('<td class="vertical-center">').append(v.name.replace('-',' ').replace('_',' ') + code_fork))
-                    .append($('<td class="vertical-center">').append(v.description))
-                    .append($('<td class="vertical-center">').append(v.updated_at.substring(0, 10)))
-                    .append($('<td>').append(
-                        $('<a>').attr({href:v.html_url, class:'btn btn-sm btn-outline-info btn-block', target:'_blank'}).html('<i class="fa fa-github"></i> Source'))
-                    )
-                );
+            tag_data = $.get("https://api.github.com/repos/mortezaipo/" + v.name + "/tags", function(tag_data) {
+                if(tag_data.length)
+                    state = "Released";
+                else
+                    state = "Dev";
+
+                code_fork = '';
+                if(v.fork == true) code_fork = ' <i class="fa fa-code-fork"></i>'
+                $('#projects-list tbody')
+                    .append($("<tr>")
+                        .append($('<td class="text-center">').append(i+1))
+                        .append($('<td>').append(v.name.replace('-',' ').replace('_',' ') + code_fork))
+                        .append($('<td>').append(v.description))
+                        .append($('<td>').append('<label class="badge badge-light">' + v.updated_at.substring(0, 10) + '</label>'))
+                        .append($('<td>').append('<label class="badge badge-light">' + state + '</label>'))
+                        .append($('<td class="text-center">').append(
+                            $('<a>').attr({href:v.html_url, class:'src-links', target:'_blank'}).html('<i class="fab fa-github"></i>'))
+                        )
+                    );
+                });
             }
         });
     });
